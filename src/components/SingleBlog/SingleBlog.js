e4d3efa281b93671d42e961e4f6123125866a0df
Soikat07 <sahasoikat94@gmail.com>
2023-03-31 11:54:35 +0600

    font awesome icon add

--- src/components/SingleBlog/SingleBlog.js
+++ src/components/SingleBlog/SingleBlog.js
@@ -1,4 +1,6 @@
 import React from 'react';
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+  import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 const SingleBlog = props => {
   const {
@@ -13,19 +15,33 @@ const SingleBlog = props => {
   return (
     <div className="mt-3">
       <div className="card w-50">
-        <img src={blog_image}className="card-img-top w-full img-fluid"alt="..."/>
+        <img
+          src={blog_image}
+          className="card-img-top w-full img-fluid"
+          alt="..."
+        />
         <div className="card-body">
           <div className="d-flex justify-content-between">
             <div className="d-flex gap-2">
               <div>
-                <img src={author_image}className="rounded-5"alt=""width={'40px'}/>
+                <img
+                  src={author_image}
+                  className="rounded-5"
+                  alt=""
+                  width={'40px'}
+                />
               </div>
               <div>
                 <h5>{author_name}</h5>
                 <p>{published_date}</p>
               </div>
             </div>
-              <p>{read_time} min read</p>
+            <p>
+              {read_time} min read{" "}
+              <a href='./marked'>
+                <FontAwesomeIcon icon={faBookmark} />
+              </a>
+            </p>
           </div>
           <h4 className="card-title">{blog_title}</h4>
           <p>#beginners #programming</p>
